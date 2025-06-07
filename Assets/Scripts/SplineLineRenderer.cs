6569e427966226fc555bb6d2afe163f186f602ec
ahsan2649 <ahsan2649@outlook.com>
2025-06-07 04:59:24 +0200

    SplineWaver, SplineLineRenderer

---
+++ Assets/Scripts/SplineLineRenderer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+[RequireComponent(typeof(SplineContainer))]
+[RequireComponent(typeof(LineRenderer))]
+public class SplineLineRenderer : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    private Spline _spline;
+
+    [SerializeField] private int numPoints;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _spline = GetComponent<SplineContainer>().Spline;
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = numPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < numPoints; i++)
+        {
+            var lerpValue = i / (float)(numPoints - 1);
+            var lerpPoint = _spline.EvaluatePosition(lerpValue);
+            _lineRenderer.SetPosition(i, transform.position + (Vector3)lerpPoint);
+        }
+    }
+}
---
+++ Assets/Scripts/SplineWaver.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Splines;
+
+[RequireComponent(typeof(SplineContainer))]
+public class SplineWaver : MonoBehaviour
+{
+    Spline _spline;
+    private Vector3[] _initialKnotPositions;
+
+    [SerializeField] private Vector3 waveDirection;
+    [SerializeField] private float frequency = 1;
+    [SerializeField] private float amplitude = 1;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private float offset = 1;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _spline = GetComponent<SplineContainer>().Spline;
+        _initialKnotPositions = new Vector3[_spline.Count];
+        for (int i = 0; i < _spline.Count; i++)
+        {
+            _initialKnotPositions[i] = _spline[i].Position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 1; i < _spline.Count - 1; i++)
+        {
+            float t = (float)i / (_spline.Count - 1);
+            float waveOffset = Mathf.Sin(t * frequency * Mathf.PI * 2f + Time.time * speed + offset) * amplitude;
+            Vector3 displacedPosition = _initialKnotPositions[i] + waveDirection.normalized * waveOffset;
+            _spline[i] = new BezierKnot(displacedPosition, _spline[i].TangentIn, _spline[i].TangentOut, _spline[i].Rotation);
+        }
+    }
+}

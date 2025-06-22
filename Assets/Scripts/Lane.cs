f26154efce60f7d41ccf1f16771c9511993875aa
Alif Ahsan Nibir <ahsan2649@outlook.com>
2025-06-23 01:15:55 +0200

    Changing Lanes to be individual

--- Assets/Scripts/Lane.cs
+++ Assets/Scripts/Lane.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Splines;
@@ -6,42 +6,71 @@ using UnityEngine.Splines;
 
 public class Lane : MonoBehaviour
 {
-    [Tooltip("Need two identical splines with opposite flows")]
-    [SerializeField] private SplineContainer forwardSpline;
-
-    [Tooltip("Need two identical splines with opposite flows")]
-    [SerializeField] private SplineContainer backwardSpline;
+    [Tooltip("The spline to use for animating notes.")]
+    [SerializeField] private SplineContainer spline;
     
     [Tooltip("The prefab to spawn as a note. Must have a Note component attached.")]
     [SerializeField] private GameObject notePrefab;
-    
+
+    readonly List<Note> _notes = new List<Note>();
+
+    [Tooltip("The key that triggers a hit on the note")]
+    [SerializeField] private InputAction key;
+
+    private void OnEnable()
+    {
+        key.Enable();
+    }
+
+    private void Awake()
+    {
+        key.performed += Hit;
+    }
+
+    private void OnDisable()
+    {
+        key.Disable();
+        
+    }
+
     private void Update()
     {
 #if UNITY_EDITOR
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            SpawnNote(1);
-        }
-        
-        if (Keyboard.current.enterKey.wasPressedThisFrame)
-        {
-            SpawnNote(1, true);
+            SpawnNote(5);
         }
 #endif
+        
+    }
+
+    
+    // Called on keypress
+    // Tells the note to despawn
+    void Hit(InputAction.CallbackContext context)
+    {
+        var note = _notes[0];
+        _notes.RemoveAt(0);
+        note.OnNoteHit?.Invoke();
         
     }
 
     // Call this function to spawn a note.
     // Duration means how long the note takes to go from one end to the other
     // Reverse means it starts from the other direction (the reverse spline)
-    void SpawnNote(float duration, bool reverse = false)
+    void SpawnNote(float duration)
     {
-        var targetContainer = reverse ? forwardSpline : backwardSpline;
+        var noteGameObject = Instantiate(notePrefab, spline.Spline.EvaluatePosition(0), spline.transform.rotation);
         
-        var note = Instantiate(notePrefab, targetContainer.Spline.EvaluatePosition(0), targetContainer.transform.rotation);
-        var splineAnimate = note.GetComponent<SplineAnimate>();
-        splineAnimate.Container = targetContainer;
+        var note =  noteGameObject.GetComponent<Note>();
+        _notes.Add(note);
+        
+        var splineAnimate = noteGameObject.GetComponent<SplineAnimate>();
+        splineAnimate.Container = spline;
         splineAnimate.Duration = duration;
+        
         splineAnimate.Play();
+
+        
     }
 }
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -1,21 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Splines;
 
 [RequireComponent(typeof(SplineAnimate))]
 public class Note : MonoBehaviour
 {
-    SplineAnimate _splineAnimate;
+    private SplineAnimate _splineAnimate;
+    public Action OnNoteHit;
+    
+    
     void Awake()
     {
         _splineAnimate = GetComponent<SplineAnimate>();
-        _splineAnimate.Completed += OnSplineAnimateOnCompleted;
+        _splineAnimate.Completed += SplineAnimateOnCompleted;
+        OnNoteHit += SplineAnimateOnCompleted;
     }
-
-    private void OnSplineAnimateOnCompleted()
+    
+    void SplineAnimateOnCompleted()
     {
-        _splineAnimate.Completed -= OnSplineAnimateOnCompleted;
+        _splineAnimate.Completed -= SplineAnimateOnCompleted;
+        OnNoteHit -= SplineAnimateOnCompleted;
         Destroy(gameObject);
     }
+    
+    
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

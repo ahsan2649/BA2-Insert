e2a903f376282a4c5566485186c811b44a593b40
Michael Shollaj <Michaelshollaj129@gmail.com>
2025-07-14 16:44:24 +0200

    AddedParticleSytemToNote

--- Assets/Scripts/Ahsan/Note.cs
+++ Assets/Scripts/Ahsan/Note.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -9,5 +9,23 @@ namespace Ahsan
 	{
 		public SplineAnimate splineAnimate;
 		public float hitTime;
+
+        [Header("VFX")]
+        [Tooltip("Assign a particle prefab that plays once and then auto‐destroys itself")]
+        public GameObject hitParticlePrefab;
+
+
+        private void OnDestroy()
+        {
+            // spawn the hit particle at this note's position
+            if (hitParticlePrefab != null)
+            {
+                Instantiate(
+                    hitParticlePrefab,
+                    transform.position,
+                    Quaternion.identity
+                );
+            }
+        }
     }
 }
